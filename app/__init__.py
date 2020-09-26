aab29102a7d461291b5a9909dec30ef9b9433837
Marcel Fox <marcelfox@live.com>
2020-09-26 14:02:01 -0300

    renaming app folder from src to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
 
-
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config')
 
     with app.app_context():
-        from src import Router
+        from .Core import Router
         return app
