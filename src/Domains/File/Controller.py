6541c095e6bd9714c9637da0cc368635aa471b97
Marcel Fox <marcelfox@live.com>
2020-09-26 23:39:56 -0300

    Adding getBroker and validLines broker

--- src/Domains/File/Controller.py
+++ src/Domains/File/Controller.py
@@ -16,15 +16,33 @@ class FileController:
     def checkNumbers(self, file):
         validLines = []
         for e in DictReader(file.splitlines(), delimiter=';'):
-            if len(e["DDD"]) < 2:
+            if int(e["CELULAR"][0]) != 9:
+                print(e["CELULAR"])
+                continue
+
+            if len(e["DDD"]) < 2 or len(e["CELULAR"]) != 9:
                 continue
 
             if int(e["CELULAR"][1]) < 6:
                 continue
 
+            print(e["OPERADORA"])
+            e["BROKER"] = self.getBroker(e["OPERADORA"])
+
             try:
                 self._fileService.get(f'/blacklist/{e["DDD"]}{e["CELULAR"]}')
             except HTTPError as err:
                 if err.code == 404:
-                    validLines.append(e)
+                    validLines.append(f'{e["IDMENSAGEM"]};{e["BROKER"]}')
         return jsonify(validLines)
+
+    @staticmethod
+    def getBroker(op):
+        print(op)
+        return {
+            "VIVO": 1,
+            "TIM": 1,
+            "CLARO": 2,
+            "OI": 2,
+            "NEXTEL": 3
+        }.get(op, None)
