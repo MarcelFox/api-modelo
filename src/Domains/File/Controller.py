44ba5ade05d06302e3ae2de51aa106ae14dc1c46
Marcel Fox <marcelfox@live.com>
2020-09-27 12:16:32 -0300

    Refactoring FileController

--- src/Domains/File/Controller.py
+++ src/Domains/File/Controller.py
@@ -8,34 +8,33 @@ from urllib.error import HTTPError
 class FileController:
     def __init__(self):
         self._fileService = FileService()
-    
+
     def mainPage(self):
         return jsonify({"status": 200, "message": "Main Success"})
 
     def checkNumbers(self, file):
-        validLines = []
-        for e in DictReader(file.splitlines(), delimiter=';'):
-            if int(e["CELULAR"][0]) != 9:
-                print(e["CELULAR"])
-                continue
+        numbersChecked = self.__numValidation(file)
+        validLines = self.__requestNumbers(numbersChecked)
 
-            if len(e["DDD"]) < 2 or len(e["CELULAR"]) != 9:
-                continue
-
-            if int(e["CELULAR"][1]) < 6:
-                continue
-
-            print(e["OPERADORA"])
-            e["BROKER"] = self.__getBroker(e["OPERADORA"])
-
-            try:
-                self._fileService.get(f'/blacklist/{e["DDD"]}{e["CELULAR"]}')
-            except HTTPError as err:
-                if err.code == 404:
-                    validLines.append(f'{e["IDMENSAGEM"]};{e["BROKER"]}')
         return jsonify(validLines)
 
-    def __getBroker(self, op):
+    def __numValidation(self, payload: dict):
+        validNumbers = []
+        for data in DictReader(payload.splitlines(), delimiter=';'):
+            if int(data["CELULAR"][0]) != 9:
+                continue
+
+            if len(data["DDD"]) < 2 or len(data["CELULAR"]) != 9:
+                continue
+
+            if int(data["CELULAR"][1]) < 6:
+                continue
+
+            data["BROKER"] = self.__getBroker(data["OPERADORA"])
+            validNumbers.append(data)
+        return validNumbers
+
+    def __getBroker(self, op: str):
         print(op)
         return {
             "VIVO": 1,
@@ -44,3 +43,14 @@ class FileController:
             "OI": 2,
             "NEXTEL": 3
         }.get(op, None)
+
+    def __requestNumbers(self, payload: list):
+        validLines = []
+        for data in payload:
+            try:
+                self._fileService.get(
+                    f'/blacklist/{data["DDD"]}{data["CELULAR"]}')
+            except HTTPError as err:
+                if err.code == 404:
+                    validLines.append(f'{data["IDMENSAGEM"]};{data["BROKER"]}')
+        return validLines
